10afcd0a040ac6f33037b6c8599db89e4e80ef6b
Matthew Watkins <mpwatki2@ncsu.edu>
2015-04-08 14:31:39 -0400

    Version 1.9:
    - Better logic around when to codify
    - Better, more consistent code formatting
    - Better in general

--- src/background.js
+++ src/background.js
@@ -1,43 +1,34 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) 
-{
-    if (request.method == "getSettings") 
-	{
-	   sendResponse(getSettings());
-	}
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    if (request.method == "getSettings") {
+        sendResponse(getSettings());
+    }
 });
 
-function getSettings()
-{
-	var settingsString = localStorage["btvSettings"];
-	
-	var settings;
-	if (settingsString == null || settingsString == '' || settingsString == 'undefined')
-	{
-		settings = new Object();
-		settings.doLineWrap = false;
-		settings.fontFamily = 'monospace';
-		settings.fontSize = 0;
-		setSettings(settings);
-	}
-	else
-	{
-		settings = JSON.parse(settingsString);
-	}
-	
-	return settings;
+function getSettings() {
+    var settingsString = localStorage["btvSettings"];
+
+    var settings;
+    if (settingsString == null || settingsString == '' || settingsString == 'undefined') {
+        settings = new Object();
+        settings.doLineWrap = false;
+        settings.fontFamily = 'monospace';
+        settings.fontSize = 0;
+        setSettings(settings);
+    } else {
+        settings = JSON.parse(settingsString);
+    }
+
+    return settings;
 }
 
-function setSettings(settings)
-{
-	localStorage["btvSettings"] = JSON.stringify(settings);
+function setSettings(settings) {
+    localStorage["btvSettings"] = JSON.stringify(settings);
 }
 
-function isTrue(value)
-{
-	return value == true || value == 'true';
+function isTrue(value) {
+    return value == true || value == 'true';
 }
 
-function hasValue(value)
-{
-	return value != null && value != '' && value != 'undefined' && (value.toString().length > 10 || value.toString().trim().length > 0);
+function hasValue(value) {
+    return value != null && value != '' && value != 'undefined' && (value.toString().length > 10 || value.toString().trim().length > 0);
 }
--- src/content.js
+++ src/content.js
@@ -1,166 +1,119 @@
 // Run on page load
-chrome.runtime.sendMessage({localstorage: "sync"}, run);
+chrome.runtime.sendMessage({
+    localstorage : "sync"
+}, run);
 // chrome.webRequest.onHeadersReceived.addListener(function() {alert('Headers received');});
 
+function run() {
+    var ext = getExtensionFromUrl(window.location.href);
+    var codeMode = getModeForExtension(ext);
+    console.log('Ext is ' + ext + ' and code mode is ' + codeMode);
 
-function run()
-{
-	var htmlElement = $('html');
-	if (htmlElement != null)
-	{
-		var bodyElement = $('body');
-		if (bodyElement != null)
-		{			
-			var bodyChildren = bodyElement.children();
-			if (bodyChildren != null && bodyChildren.length == 1)
-			{
-				if (bodyChildren[0].nodeName == 'PRE')
-				{
-					// This is an HTML document with a body which has one element, and that element is a pre
-					// Therefore we can be pretty sure this is Chrome viewing a plain-text file of some sort
-					// Start by guessing at which mode to use based on the file extension
-					var mode = getModeFromUrl(window.location.href);
-						
-					// Request the settings
-					chrome.extension.sendRequest({method: "getSettings"}, function(settings) 
-					{
-						// Perform the replacement
-						var editor = replaceCodeElementWithEditor(bodyChildren[0], mode, settings.doLineWrap);
-						applyStyleFromSettings(settings);
-						bodyElement.append(getAuthorLinkBoxHtml());
-					});
-				}
-			}
-		}
-	}
+    var bodyElement = $('body');
+    if (bodyElement != null) {
+        var bodyNodes = bodyElement.contents();
+        if (bodyNodes.length > 0) {
+            var contentsToChangeToCode = null;
+            var firstNode = bodyNodes[0];
+            if (bodyNodes.length == 1) {
+                if (firstNode.nodeName == '#text') {
+                    applyEditor(bodyElement, codeMode, bodyElement.html());
+                } else if (firstNode.nodeName == 'PRE') {
+					applyEditor(bodyElement, codeMode, firstNode.innerText);
+                }
+            } else if (firstNode.nodeName == '#text' && codeMode != 'plain-text') {
+                applyEditor(bodyElement, codeMode, bodyElement.html());
+            }
+        }
+    }
 }
 
-function replaceCodeElementWithEditor(codeElement, mode, doLineWrap)
-{
-	return replaceElementWithEditor($('pre')[0], codeElement.innerText, mode, doLineWrap);
+function getExtensionFromUrl(url) {
+    var ext = null;
+    if (url != null && url.length > 0) {
+        var urlParts = url.split('?')[0].split('/');
+        if (urlParts.length > 0) {
+            var lastUrlPiece = urlParts[urlParts.length - 1].split('#')[0];
+            if (lastUrlPiece.indexOf('.') >= 0) {
+                var dotPieces = lastUrlPiece.split('.');
+                ext = dotPieces[dotPieces.length - 1];
+            }
+        }
+    }
+    return ext;
 }
 
-function replaceElementWithEditor(element, value, mode, doLineWrap)
-{
-	// Setup options
-	var options = 
-	{
-		value: value,
-		lineNumbers: true,
-		fullScreen: true,
-		mode: 'plain-text',
-		lineWrapping: doLineWrap
-	};
-		
-	// Overwrite the mode
-	if (mode != null && mode.length > 1)
-	{
-		options.mode = mode;
-		if (options.mode == "clike")
-	    {
-	      options.useCPP = true;
-	    }
-	}
-	
-	// Construct the editor
-	var myCodeMirror = CodeMirror(function(elt)
-	{
-	  element.parentNode.replaceChild(elt, element);
-	}, 
-	options
-	);
-	
-	return myCodeMirror;
+function getModeForExtension(ext) {
+    var mode = 'plain-text';
+    if (ext != null) {
+        ext = ext.trim().toLowerCase();
+        switch (ext) {
+        case 'xml':
+        case 'csproj':
+            mode = 'xml';
+            break;
+        case 'html':
+            mode = 'html';
+            break;
+        case 'cshtml':
+        case 'aspx':
+            mode = 'htmlmixed';
+            break;
+        case 'cs':
+        case 'java':
+        case 'c':
+            mode = 'clike';
+            break;
+        case 'sql':
+            mode = 'sql'
+                break;
+        case 'js':
+        case 'json':
+            mode = 'javascript';
+            break;
+        case 'css':
+            mode = 'css';
+            break;
+        default:
+            break;
+        }
+    }
+
+    return mode;
 }
 
-function applyStyleFromSettings(settings)
-{
-	// Settings overrides
-	if (settings.fontFamily != 'monospace' || settings.fontSize > 0)
-	{
-		if (settings.fontFamily != 'monospace' && settings.fontSize > 0)
-		{
-			// override both properties
-			$('.CodeMirror').css('font-family', settings.fontFamily).css('font-size', settings.fontFamily);
-		}
-		else
-		{
-			if (settings.fontFamily != 'monospace')
-			{
-				$('.CodeMirror').css('font-family', settings.fontFamily);
-			}
-			else
-			{
-				$('.CodeMirror').css('font-size', settings.fontSize + 'px');
-			}
-		}
-	}
+function applyEditor(bodyElement, codeMode, content) {
+    chrome.extension.sendRequest({
+        method : "getSettings"
+    }, function (settings) {
+        // var editor = CodeMirror(document.body, {value: content});
+        var editor = CodeMirror(function (codeEditorElement) {
+                bodyElement.html(codeEditorElement)
+            }, {
+                value : content,
+                lineNumbers : true,
+                fullScreen : true,
+                mode : 'plain-text',
+                lineWrapping : settings.doLineWrap,
+                mode : codeMode,
+                useCPP : (codeMode == "clike")
+            });
+        applyStyleFromSettings(settings);
+    });
 }
 
-function getAuthorLinkBoxHtml()
-{
-	return '<div id="authorLinkBox" class="authorLinkBox">'
-	 + '<span id="likeMessage">Like this extension? Click <a class="rateLink" href="https://chrome.google.com/webstore/detail/better-text-viewer/lcaidopdffhfemoefoaadecppnjdknkc" target="_blank">here</a> to rate it</span>'
-	 + '<span><a href="#" class="closeLink" onClick="document.getElementById(\'authorLinkBox\').style.display = \'none\';">x</a></span>'
-	 + '</div>'
-}
-
-function getModeFromUrl(url)
-{
-	var mode = 'plain-text';
-	if (url != null && url.length > 0)
-	{
-		var urlParts = url.split('?')[0].split('.');
-		if (urlParts != null && urlParts.length > 0)
-		{
-			var extensionPiece = urlParts[urlParts.length - 1];
-			if (extensionPiece != null && extensionPiece.length > 0)
-			{
-				extensionPiece = extensionPiece.replace('#', '').replace('/', '');
-				if (extensionPiece.length > 0)
-				{
-					mode = getModeForExtension(extensionPiece);
-				}
-			}
-		}
-	}
-	return mode;
-}
-
-function getModeForExtension(ext)
-{
-	var mode = 'plain-text';
-	ext = ext.trim().toLowerCase();
-	switch (ext)
-	{
-		case 'xml':
-		case 'csproj':
-			mode = 'xml';
-			break;
-		case 'html':
-			mode = 'html';
-			break;
-		case 'cshtml':
-		case 'aspx':
-			mode = 'htmlmixed';
-			break;
-		case 'cs':
-		case 'java':
-		case 'c':
-			mode = 'clike';
-			break;
-		case 'sql':
-			mode = 'sql'
-			break;
-		case 'js':
-		case 'json':
-			mode = 'javascript';
-			break;
-		case 'css':
-			mode = 'css';
-			break;
-		default:
-			break;
-	}
-	return mode;
+function applyStyleFromSettings(settings) {
+    // Settings overrides
+    if (settings.fontFamily != 'monospace' || settings.fontSize > 0) {
+        if (settings.fontFamily != 'monospace' && settings.fontSize > 0) {
+            // override both properties
+            $('.CodeMirror').css('font-family', settings.fontFamily).css('font-size', settings.fontFamily);
+        } else {
+            if (settings.fontFamily != 'monospace') {
+                $('.CodeMirror').css('font-family', settings.fontFamily);
+            } else {
+                $('.CodeMirror').css('font-size', settings.fontSize + 'px');
+            }
+        }
+    }
 }
