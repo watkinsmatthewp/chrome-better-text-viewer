77f5ecd9f0a62156adce39682c77b6932d7aaa96
Matthew Watkins <mpwatki2@ncsu.edu>
2015-05-14 12:18:25 -0400

    Message less ugly

--- src/Settings.js
+++ src/Settings.js
@@ -5,6 +5,7 @@ $(window).load(function () {
     // Attach an event to the reset button
     $('#reset').click(function() {
         showSettings(saveSettings(defaultSettings()));
+        showAlertMessage('Settings have been reset to default values');
     });
     
     // Attach an event to the save button
@@ -19,10 +20,25 @@ $(window).load(function () {
         var validation = validateAndAutoCorrectSettings(newSettings);
         if (validation.isValid) {
             showSettings(saveSettings(validation.settings));
-            alert('Settings saved');
+            showAlertMessage('Settings saved');
+        } else {
+            showAlertMessage('Please correct errors and hit save again', true);
         }
     });
 });
+
+// Display the alert div (temporarily or permanently for a specified amount of time)
+function showAlertMessage(text, keepAlive, aliveTime) {
+	keepAlive = keepAlive || false;
+    aliveTime = aliveTime || 3000;
+    
+    var alertBox = $('#alert');
+	alertBox.text(text);
+	alertBox.fadeTo(400, 1);
+    if (!keepAlive) {
+      alertBox.delay(aliveTime).fadeTo(1000, 0);
+    }
+}
 
 // Restores select box state to saved value from localStorage.
 function showSettings(settings) {
